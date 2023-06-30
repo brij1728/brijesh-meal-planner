1f6287d2bb3eda3fced34d282c45736bb737f4fc
Brijesh Kumar <bk10895@gmail.com>
2023-06-30 09:49:12 +0530

    feat: add category title to MealsOverviewScreens and update dependencies
    - Introduce useLayoutEffect hook to set the title of the screen based on the selected category
    - Update imports in MealsOverviewScreens to useLayoutEffect and CATEGORIES
    - Remove unused Text component from MealsOverviewScreens
    - Update FlatList keyExtractor to use item.id

--- src/screens/MealsOverviewScreens.tsx
+++ src/screens/MealsOverviewScreens.tsx
@@ -1,18 +1,30 @@
-import { FlatList, ListRenderItemInfo, Text, View } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { FlatList, ListRenderItemInfo, View } from 'react-native';
 
 import { MealItem } from '../components';
-import { MEALS } from '../fixtures';
+import { CATEGORIES, MEALS } from '../fixtures';
 import { MealsOverviewScreenProps } from '../stackroutes';
 import { Meal } from '../types';
 
 export const MealsOverviewScreens: React.FC<MealsOverviewScreenProps> = ({
   route,
+  navigation,
 }) => {
   const categoryId = route.params.categoryId;
 
   const displayMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0,
   );
+
+  const categoryTitle = CATEGORIES.find(
+    (category) => category.id === categoryId,
+  )?.title;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [categoryId, categoryTitle, navigation]);
 
   const renderMealItem = ({ item }: ListRenderItemInfo<Meal>) => {
     return (
@@ -29,7 +41,6 @@ export const MealsOverviewScreens: React.FC<MealsOverviewScreenProps> = ({
 
   return (
     <View>
-      <Text>MealsOverviewScreens -{categoryId}</Text>
       <FlatList
         data={displayMeals}
         keyExtractor={(item) => item.id}
