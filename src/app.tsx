3c61383e378bda8facb2372c1129dc484dec2580
Brijesh Kumar <bk10895@gmail.com>
2023-08-19 11:37:24 +0530

    feat(AndroidManifest): Ensure status bar visibility and streamline app theme
    - Modified activity themes to improve status bar appearance.
    - Checked and adjusted React Native status bar configurations.
    - Ensured consistency of visibility settings across different app views.
    - Validated changes on multiple Android devices for uniform behavior.

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,3 @@
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider, lightTheme } from './theme';
 
 import { AppNavigation } from './navigation';
@@ -9,15 +8,13 @@ import { store } from './store';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" />
-        <Provider store={store}>
-          <ThemeProvider initialTheme={lightTheme}>
-            <AppNavigation />
-          </ThemeProvider>
-        </Provider>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <StatusBar barStyle="light-content" hidden={false} />
+      <Provider store={store}>
+        <ThemeProvider initialTheme={lightTheme}>
+          <AppNavigation />
+        </ThemeProvider>
+      </Provider>
+    </>
   );
 }
